80ae0b4888e835458216ef79e0b8e8b93af90af8
Sergey Gogolev <gogoleff@yandex-team.ru>
2016-10-24 16:00:25 +0500

    Добавлена задача

---
+++ lego.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/**
+ * Сделано задание на звездочку
+ * Реализованы методы or и and
+ */
+exports.isStar = true;
+
+/**
+ * Запрос к коллекции
+ * @param {Array} collection
+ * @params {...Function} – Функции для запроса
+ * @returns {Array}
+ */
+exports.query = function (collection) {
+    return collection;
+};
+
+/**
+ * Выбор полей
+ * @params {...String}
+ */
+exports.select = function () {
+    return;
+};
+
+/**
+ * Фильтрация поля по массиву значений
+ * @param {String} property – Свойство для фильтрации
+ * @param {Array} values – Доступные значения
+ */
+exports.filterIn = function (property, values) {
+    console.info(property, values);
+
+    return;
+};
+
+/**
+ * Сортировка коллекции по полю
+ * @param {String} property – Свойство для фильтрации
+ * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ */
+exports.sortBy = function (property, order) {
+    console.info(property, order);
+
+    return;
+};
+
+/**
+ * Форматирование поля
+ * @param {String} property – Свойство для фильтрации
+ * @param {Function} formatter – Функция для форматирования
+ */
+exports.format = function (property, formatter) {
+    console.info(property, formatter);
+
+    return;
+};
+
+/**
+ * Ограничение количества элементов в коллекции
+ * @param {Number} count – Максимальное количество элементов
+ */
+exports.limit = function (count) {
+    console.info(count);
+
+    return;
+};
+
+if (exports.isStar) {
+
+    /**
+     * Фильтрация, объединяющая фильтрующие функции
+     * @star
+     * @params {...Function} – Фильтрующие функции
+     */
+    exports.or = function () {
+        return;
+    };
+
+    /**
+     * Фильтрация, пересекающая фильтрующие функции
+     * @star
+     * @params {...Function} – Фильтрующие функции
+     */
+    exports.and = function () {
+        return;
+    };
+}
